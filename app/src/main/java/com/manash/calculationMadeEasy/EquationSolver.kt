9d71f4bd8b463b11361ab740916ca21ecc988dbd
MANASH DAS <manashdasdmj5@gmail.com>
2021-11-09 01:45:32 +0530

    Equation solver is reformatted

--- app/src/main/java/com/manash/calculationMadeEasy/EquationSolver.kt
+++ app/src/main/java/com/manash/calculationMadeEasy/EquationSolver.kt
@@ -1,5 +1,4 @@
 package com.manash.calculationMadeEasy
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,7 +10,6 @@ import com.chaquo.python.PyObject
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
 import kotlinx.android.synthetic.main.activity_equation_solver.*
-import kotlinx.android.synthetic.main.activity_main.*
 import org.mariuszgromada.math.mxparser.Expression
 
 class EquationSolver : AppCompatActivity() {
@@ -26,12 +24,9 @@ class EquationSolver : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_equation_solver)
-        input_text_equation_solver.showSoftInputOnFocus=false
-        // Create an ArrayAdapter using the string array and a default spinner layout
+        answerBoxEquationSolver.showSoftInputOnFocus = false
         ArrayAdapter.createFromResource(this, R.array.Menu, android.R.layout.simple_spinner_item).also { adapter ->
-            // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
             spinnerEquationSolver.adapter = adapter
         }
         spinnerEquationSolver.setSelection(2)
@@ -39,7 +34,7 @@ class EquationSolver : AppCompatActivity() {
         spinnerEquationSolver.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
 
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                var intent:Intent
+                val intent:Intent
                 when (p2) {
                     0->{
                         intent=Intent(this@EquationSolver,MainActivity::class.java)
@@ -59,38 +54,49 @@ class EquationSolver : AppCompatActivity() {
 
             }
         }
+
+        zeroEquationSolver.setOnClickListener{updateTextEquationSolver("0")}
+        oneEquationSolver.setOnClickListener{updateTextEquationSolver("1")}
+        twoEquationSolver.setOnClickListener{updateTextEquationSolver("2")}
+        threeEquationSolver.setOnClickListener{updateTextEquationSolver("3")}
+        fourEquationSolver.setOnClickListener{updateTextEquationSolver("4")}
+        fiveEquationSolver.setOnClickListener{updateTextEquationSolver("5")}
+        sixEquationSolver.setOnClickListener{updateTextEquationSolver("6")}
+        sevenEquationSolver.setOnClickListener{updateTextEquationSolver("7")}
+        eightEquationSolver.setOnClickListener{updateTextEquationSolver("8")}
+        nineEquationSolver.setOnClickListener{updateTextEquationSolver("9")}
+
+        pointEquationSolver.setOnClickListener{updateTextEquationSolver(".")}
+        openBracketEquationSolver.setOnClickListener{updateTextEquationSolver("(")}
+        closeBracketEquationSolver.setOnClickListener{updateTextEquationSolver(")")}
+        squareRootEquationSolver.setOnClickListener { updateTextEquationSolver("sqrt(") }
+
+        plusEquationSolver.setOnClickListener{updateTextEquationSolver("+")}
+        minusEquationSolver.setOnClickListener{updateTextEquationSolver("-")}
+        multiplyEquationSolver.setOnClickListener{updateTextEquationSolver("*")}
+        divideEquationSolver.setOnClickListener{updateTextEquationSolver("/")}
+        backspaceEquationSolver.setOnClickListener{backspaceBTNEquationSolver()}
+        clearEquationSolver.setOnClickListener { clrBTNEquationSolver() }
+
+        variableEquationSolver.setOnClickListener { varBTNEquationSolver() }
+        degreeEquationSolver.setOnClickListener { degreeBTNEquationSolver() }
+        polynomialEquationSolver.setOnClickListener { polynomialSolutionBTN() }
+        LinearEqnEquationSolver.setOnClickListener { variableSolutionBTN() }
     }
     override fun onStart() {
         super.onStart()
         spinnerEquationSolver.setSelection(2)
     }
-    @SuppressLint("SetTextI18n")
     private fun updateTextEquationSolver(txtToAdd: String){
-        Log.d("updateTextEquationSolver",txtToAdd)
         if (switch=="variable" || switch=="degree"){
-            val cursorPos: Int = answer_box_equation_solver.selectionStart
-            answer_box_equation_solver.setText(answer_box_equation_solver.text.insert(cursorPos, txtToAdd).toString())
-            answer_box_equation_solver.setSelection(cursorPos + txtToAdd.length)
+            val cursorPos: Int = answerBoxEquationSolver.selectionStart
+            answerBoxEquationSolver.setText(answerBoxEquationSolver.text.insert(cursorPos, txtToAdd).toString())
+            answerBoxEquationSolver.setSelection(cursorPos + txtToAdd.length)
         }
     }
-    fun zeroBTN(@Suppress("UNUSED_PARAMETER")view: View) { updateTextEquationSolver("0") }
-    fun oneBTN(@Suppress("UNUSED_PARAMETER")view: View) { updateTextEquationSolver("1") }
-    fun twoBTN(@Suppress("UNUSED_PARAMETER")view: View) { updateTextEquationSolver("2") }
-    fun threeBTN(@Suppress("UNUSED_PARAMETER")view: View) { updateTextEquationSolver("3") }
-    fun fourBTN(@Suppress("UNUSED_PARAMETER")view: View) { updateTextEquationSolver("4") }
-    fun fiveBTN(@Suppress("UNUSED_PARAMETER")view: View) { updateTextEquationSolver("5") }
-    fun sixBTN(@Suppress("UNUSED_PARAMETER")view: View) { updateTextEquationSolver("6") }
-    fun sevenBTN(@Suppress("UNUSED_PARAMETER")view: View) { updateTextEquationSolver("7") }
-    fun eightBTN(@Suppress("UNUSED_PARAMETER")view: View) { updateTextEquationSolver("8") }
-    fun nineBTN(@Suppress("UNUSED_PARAMETER")view: View) { updateTextEquationSolver("9") }
-    fun pointBTN(@Suppress("UNUSED_PARAMETER")view: View) { updateTextEquationSolver(".") }
-    fun minusBTN(@Suppress("UNUSED_PARAMETER")view: View) { updateTextEquationSolver("-") }
-    fun plusBTN(@Suppress("UNUSED_PARAMETER")view: View) { updateTextEquationSolver("+") }
-    fun multiplyBTN(@Suppress("UNUSED_PARAMETER")view: View) { updateTextEquationSolver("*")}
-    fun divideBTN(@Suppress("UNUSED_PARAMETER")view: View) { updateTextEquationSolver("/") }
-    fun clrBTN(@Suppress("UNUSED_PARAMETER")view: View) {
-        input_text_equation_solver.setText("")
-        answer_box_equation_solver.setText("")
+    private fun clrBTNEquationSolver() {
+        inputTextEquationSolver.text = ""
+        answerBoxEquationSolver.setText("")
         checkValidity=0
         elements= 0
         checkNoOfElement=0
@@ -99,53 +105,51 @@ class EquationSolver : AppCompatActivity() {
         equation=""
         switch="None"
     }
-    fun backspaceBTN(@Suppress("UNUSED_PARAMETER")view: View) {
-        val oldStr:String =answer_box_equation_solver.text.toString()
-        val cursorPos:Int=answer_box_equation_solver.selectionStart
+    private fun backspaceBTNEquationSolver() {
+        val oldStr:String =answerBoxEquationSolver.text.toString()
+        val cursorPos:Int=answerBoxEquationSolver.selectionStart
         val leftStr: String= oldStr.subSequence(0,cursorPos).toString()
         if(leftStr.isEmpty()){
             return
         }
         val rightStr:String=oldStr.subSequence(cursorPos,oldStr.length).toString()
-        answer_box_equation_solver.setText(String.format("%s%s",leftStr.dropLast(1),rightStr))
-        if(answer_box_equation_solver.text.toString().isNotEmpty()) {
-            answer_box_equation_solver.setSelection(cursorPos - 1)
+        answerBoxEquationSolver.setText(String.format("%s%s",leftStr.dropLast(1),rightStr))
+        if(answerBoxEquationSolver.text.toString().isNotEmpty()) {
+            answerBoxEquationSolver.setSelection(cursorPos - 1)
         }
         else{
-            answer_box_equation_solver.setSelection(0)
+            answerBoxEquationSolver.setSelection(0)
         }
     }
-    @SuppressLint("SetTextI18n")
-    fun varBTN(@Suppress("UNUSED_PARAMETER")view: View) {
-        input_text_equation_solver.setText("Unknowns?")
+    private fun varBTNEquationSolver() {
+        inputTextEquationSolver.text = getString(R.string.unknown)
         checkValidity=1
-        polynomial.isClickable=false
-        polynomial.visibility=View.GONE
-        LinearEqn.visibility=View.VISIBLE
-        LinearEqn.isClickable=true
+        polynomialEquationSolver.isClickable=false
+        polynomialEquationSolver.visibility=View.GONE
+        LinearEqnEquationSolver.visibility=View.VISIBLE
+        LinearEqnEquationSolver.isClickable=true
         switch="variable"
     }
-    @SuppressLint("SetTextI18n")
-    fun variableSolutionBTN(@Suppress("UNUSED_PARAMETER")view: View){
+    private fun variableSolutionBTN(){
         if(checkValidity==1) {
-            elements = answer_box_equation_solver.text.toString().toInt()
-            answer_box_equation_solver.setText("")
+            elements = answerBoxEquationSolver.text.toString().toInt()
+            answerBoxEquationSolver.setText("")
             checkValidity=2
-            input_text_equation_solver.setText(alphabet[index++].plus(number.toString()))
+            inputTextEquationSolver.text = alphabet[index++].plus(number.toString())
         }
         else if(checkNoOfElement==elements*(elements+1)){
-            input_text_equation_solver.setText(equation)
+            inputTextEquationSolver.text = equation
             equation=equation.drop(1)
-            answer_box_equation_solver.setText(mathematicsPython(equation,elements+1))
+            answerBoxEquationSolver.setText(mathematicsPython(equation,elements+1))
         }
         else{
             checkNoOfElement++
-            var equationSolved=mathematicsModule(answer_box_equation_solver.text.toString())
+            val equationSolved=mathematicsModule(answerBoxEquationSolver.text.toString())
             Log.d("equationSolved",equationSolved)
             equation=equation.plus(",${equationSolved}")
             Log.d("equation",equation)
-            answer_box_equation_solver.setText("")
-            input_text_equation_solver.setText(alphabet[index++].plus(number.toString()))
+            answerBoxEquationSolver.setText("")
+            inputTextEquationSolver.text= alphabet[index++].plus(number.toString())
             if(index>=elements+1){
                 number++
                 index=0
@@ -156,44 +160,44 @@ class EquationSolver : AppCompatActivity() {
         if(!Python.isStarted()){ Python.start(AndroidPlatform(this)) }
         val py: Python = Python.getInstance()
         val pyObj: PyObject =py.getModule("Python file")
-        if(switch=="variable") {
+        return if(switch=="variable") {
             val obj: PyObject = pyObj.callAttr("polynomial", userExp, noOfElement)
             Log.d("python", obj.toString())
-            return obj.toString()
-        }
-        else{
+            obj.toString()
+        } else{
             val obj: PyObject = pyObj.callAttr("degree", userExp, noOfElement)
             Log.d("python", obj.toString())
-            return obj.toString()
+            obj.toString()
         }
     }
-    @SuppressLint("SetTextI18n")
-    fun degreeBTN(view: android.view.View) {
-        input_text_equation_solver.setText("Highest degree?")
+    private fun degreeBTNEquationSolver() {
+        inputTextEquationSolver.text= getString(R.string.highestDegree)
         checkValidity=1
-        polynomial.isClickable=true
-        polynomial.visibility=View.VISIBLE
-        LinearEqn.visibility=View.GONE
-        LinearEqn.isClickable=false
+        polynomialEquationSolver.isClickable=true
+        polynomialEquationSolver.visibility=View.VISIBLE
+        LinearEqnEquationSolver.visibility=View.GONE
+        LinearEqnEquationSolver.isClickable=false
         switch="degree"
     }
-    fun polynomialSolutionBTN(view: android.view.View) {
-        if(checkValidity==1) {
-            elements = answer_box_equation_solver.text.toString().toInt()+1
-            answer_box_equation_solver.setText("")
-            checkValidity=2
-            input_text_equation_solver.setText(alphabet[index++].plus("?"))
-        }
-        else if(elements==0){
-            input_text_equation_solver.setText("The value of variables are")
-            equation=equation.drop(1)
-            answer_box_equation_solver.setText(mathematicsPython(equation,elements+1))
-        }
-        else{
-            elements--
-            equation=equation.plus(",${answer_box_equation_solver.text}")
-            answer_box_equation_solver.setText("")
-            input_text_equation_solver.setText(alphabet[index++].plus("?"))
+    private fun polynomialSolutionBTN() {
+        when {
+            checkValidity==1 -> {
+                elements = answerBoxEquationSolver.text.toString().toInt()+1
+                answerBoxEquationSolver.setText("")
+                checkValidity=2
+                inputTextEquationSolver.text = alphabet[index++].plus("?")
+            }
+            elements==0 -> {
+                inputTextEquationSolver.text = getString(R.string.displayOutput)
+                equation=equation.drop(1)
+                answerBoxEquationSolver.setText(mathematicsPython(equation,elements+1))
+            }
+            else -> {
+                elements--
+                equation=equation.plus(",${answerBoxEquationSolver.text}")
+                answerBoxEquationSolver.setText("")
+                inputTextEquationSolver.text = alphabet[index++].plus("?")
+            }
         }
     }
     private fun mathematicsModule(userExpression: String): String {
@@ -226,6 +230,4 @@ class EquationSolver : AppCompatActivity() {
         }
         return result
     }
-    fun squareRoot(view: android.view.View) { updateTextEquationSolver("sqrt(")}
-    fun closeBTN(view: android.view.View) {updateTextEquationSolver(")")}
 }
